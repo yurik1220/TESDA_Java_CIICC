f574f572b087fad01cc1aa9134b21290681078d2
Julian <molina.315450@caloocan.sti.edu.ph>
2026-06-06 15:02:49 +0800

    Task 3

--- src/Task3.java
+++ src/Task3.java
@@ -1,14 +1,17 @@
 public class Task3 {
     static void main() {
         String a = new String("Wow");
-        String b = a;
-        String c = "munggo";
-        String d = "Wow!";
+        String b = "Wow";
+        String c = a;
+        String d = c;
 
-        boolean b1 = a == b;
+        boolean b1 = a == d;
         boolean b2 = d.equals(b + "!");
-        boolean b3 = !c.equals(a);
+        boolean b3 = c.equals(a);
 
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(b3);
         if(b1 && b2 && b3) {
             System.out.println("Success!");
         }
