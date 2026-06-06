57a87580d02e43b082d727351dea9d6698ff707f
Julian <molina.315450@caloocan.sti.edu.ph>
2026-06-06 09:29:37 +0800

    Task push

---
+++ src/Task2.java
@@ -0,0 +1,19 @@
+public class Task2 {
+    static void main() {
+        char etch = 'h';
+        byte eh = 3;
+        short djKhaled = 1;
+        int sampu = 10;
+
+        char dabolyu = 'w';
+        byte siro = 0;
+        char ar = 'r';
+        char dee = 'd';
+
+        float dos = 2.0f;
+        boolean totoo = true;
+
+        String resulta = "" + etch + eh + djKhaled + sampu + " " + dabolyu + siro + ar + djKhaled + dee + " " + dos + " " + totoo;
+        System.out.println(resulta);
+    }
+}
